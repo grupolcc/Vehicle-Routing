ac00792d7d6d2907e471ac459c1ef1d80e67955b
Kamil Łepek <kamil.lepek94@gmail.com>
2018-12-01 18:31:40 +0100

    Less requirements for passwords
    Closes #17

--- VehicleRouting/App_Start/IdentityConfig.cs
+++ VehicleRouting/App_Start/IdentityConfig.cs
@@ -50,10 +50,10 @@ namespace VehicleRouting
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
-                RequireUppercase = true,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
